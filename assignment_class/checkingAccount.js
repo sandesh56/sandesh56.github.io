f050e3de676cf87bce65c03febe97b2d0e869e5c
sandesh56 <sandeshrauth644@gmail.com>
2022-11-14 23:54:03 -0600

    use strict added

--- assignment_class/checkingAccount.js
+++ assignment_class/checkingAccount.js
@@ -1,3 +1,4 @@
+"use strict";
 class CheckingAccount extends Account {
   constructor(number, overdraft) {
     super(number);
--- assignment_class/savingsAccount.js
+++ assignment_class/savingsAccount.js
@@ -1,3 +1,4 @@
+"use strict";
 class SavingsAccount extends Account {
   constructor(number, interest) {
     super(number);
