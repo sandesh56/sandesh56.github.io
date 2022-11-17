bcf25e843b8874b7d8764bb7dea7f034864bacd1
sandesh56 <sandeshrauth644@gmail.com>
2022-11-16 22:54:01 -0600

    mathdefined

---
+++ assignment_nodejs/third/my_math.js
@@ -0,0 +1,14 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const pi = () => Math.pi;
+
+module.exports = {
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+  pi: pi,
+};
