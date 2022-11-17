b47de5323882c02cfc69e2ea443900ebfcb795ef
sandesh56 <sandeshrauth644@gmail.com>
2022-11-16 22:59:05 -0600

    module imported and used

--- assignment_nodejs/third/my_math.js
+++ assignment_nodejs/third/my_math.js
@@ -3,7 +3,7 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const pi = () => Math.pi;
+const pi = Math.PI;
 
 module.exports = {
   add: add,
