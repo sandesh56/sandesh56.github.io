5174c7f3ef93cde4e4978e79a59e1e466c2e964e
sandesh56 <sandeshrauth644@gmail.com>
2022-11-17 20:45:23 -0600

    [[feat]] dynamic css added

--- W3D5/q3/index.js
+++ W3D5/q3/index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use("/css", express.static(path.join(__dirname, "templates/style")));
+/**
+ * 
+ * middleware
+ */
+app.use("/css", express.static(path.join(__dirname, 'templates','style')));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "templates"));
+
+
+/**
+ * 
+ * Routing and middleware
+ */
 app.use("/form", (req, res) => {
   const date = new Date();
   const hour = date.getHours();
-
+  console.log(__dirname+"templates/style")
   const cssLink = hour > 18 || hour < 6 ? "/css/night.css" : "/css/day.css";
   res.render("form.ejs", { csslink: cssLink });
 });
