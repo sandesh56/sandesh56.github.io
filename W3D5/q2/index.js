82258a8f8c64a38704ebcc55db09a5b0180b936f
sandesh56 <sandeshrauth644@gmail.com>
2022-11-17 18:32:12 -0600

    [[feat]] defined all the method for q2

--- W3D5/q2/index.js
+++ W3D5/q2/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
-
-const logger = require("morgan");
-
+const path = require("path");
 const app = express();
-app.use(logger());
+const bodyParser = require("body-parser");
 
-app.use("/", (req, res) => {});
+/**
+ * Middleware
+ */
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "templates"));
+
+/**
+ *
+ * Routes
+ */
+
+app.use(express.urlencoded());
+app.get("/", (req, res) => {
+  res.render("userForm.ejs");
+});
+
+app.use("/result", (req, res) => {
+  let data = req.body;
+  res.send(`Hello ${data?.name} you are now ${data?.age}`);
+});
 
 app.listen(3000);
